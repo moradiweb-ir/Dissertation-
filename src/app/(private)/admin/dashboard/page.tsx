e209a5e0d6463c8342ec468221947b767a5a929b
sathyaprakash195 <sathyaprakash195@gmail.com>
2024-09-12 11:57:59 +0530

    section-2

---
+++ src/app/(private)/admin/dashboard/page.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { UserButton } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
+
+async function DashboardPage() {
+  const user = await currentUser();
+  console.log(user);
+  return (
+    <div className="p-5 flex flex-col gap-5">
+      <h1>Dashboard page</h1>
+      <UserButton />
+
+      <h1>
+        Name : {user?.firstName} {user?.lastName}
+      </h1>
+
+      <h1>Id : {user?.id}</h1>
+
+      <h1>Email : {user?.emailAddresses[0].emailAddress}</h1>
+    </div>
+  );
+}
+
+export default DashboardPage;
